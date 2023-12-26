cef6c91418e356302e086f27a00fffa495e8164f
Taylor Coffelt <taylorcoffelt@gmail.com>
2023-12-26 09:42:00 -0700

    Update to handle booting with ts

--- src/application.ts
+++ src/application.ts
@@ -36,7 +36,7 @@ export class LoopbackApplication extends BootMixin(
       controllers: {
         // Customize ControllerBooter Conventions here
         dirs: ['controllers'],
-        extensions: ['.controller.js'],
+        extensions: ['.controller.ts'],
         nested: true,
       },
     };
